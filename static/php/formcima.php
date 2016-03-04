a896eaa7d7452e9449b3dd5f216fd9d87f9f6515
audiomaya <audiomaya@hotmail.com>
2016-03-04 10:30:07 -0600

    Polymer
    Carga de componentes polymer necesarios para crear el nuevo sitio

--- static/php/formcima.php
+++ static/php/formcima.php
@@ -1,28 +1,18 @@
 <?php
+	$destino = "contacto@cimadistribucion.com.mx";
+	$nombre = $_POST ["nombre"];
+	$asunto = "Sitio Cima enviado por $nombre: ".$_POST["asunto"];
+	$empresa = $_POST ["empresa"];
+	$direccion = $_POST ["direccion"];
+	$telefono = $_POST ["telefono"];
+	$ferreteria = $_POST ["ferreteria"];
+	$electrico = $_POST ["electrico"];
+	$ingenieros = $_POST ["ingenieros"];
+	$mantenimiento = $_POST ["mantenimiento"];
+	$cisternas = $_POST ["cisternas"];
+	$comentarios = $_POST ["comentarios"];
+	$contenido = "Nombre:".$nombre."\nAsunto:".$asunto."\nEmpresa:".$empresa."\nDireccion:".$direccion."\nTelefono:".$telefono."\nFerreteria:".$ferreteria."\nElectrico:".$electrico."\nIngenieros:".$ingenieros."\nMantenimiento:".$mantenimiento."\nCisternas:".$cisternas."\nComentarios:".$comentarios;
 
-$para = "audiomaya@hotmail.com";
-$nombre = $_POST ["nombre"];
-$asunto = "Sitio Cima enviado por $nombre: ".$_POST["asunto"];
-$empresa = $_POST ["empresa"];
-$direccion = $_POST ["direccion"];
-$telefono = $_POST ["telefono"];
-$ferreteria = $_POST ["ferreteria"];
-$electrico = $_POST ["electrico"];
-$ingenieros = $_POST ["ingenieros"];
-$mantenimiento = $_POST ["mantenimiento"];
-$cisternas = $_POST ["cisternas"];
-$comentarios = $_POST ["comentarios"];
-$de = $_POST ["mail"];
-
-$headers = "MIME-Version:1.0;\r\n";
-$headers .= "Content-type: text/html; \r\n charset=UTF-8; \r\n";
-$headers .= "From: $de \r\n";
-$headers .= "To: $para; \r\n Subject:$asunto; \r\n";
-
-if(mail($para,$nombre,$asunto,$empresa,$direccion,$telefono,$ferreteria,$electrico,$ingenieros,$mantenimiento,$cisternas,$comentarios,$headers))
-	echo "Enviado correctamente";
-else
-	echo "Fallo en envio";
-
-
+	mail($destino,"Contacto", $contenido);
+	header("Location:/index.html");
 ?>
